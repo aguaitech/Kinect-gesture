5f371a48d1159334c345cf63b1b804df104b62b2
z-jack <emailjiong@126.com>
2021-11-28 22:32:04 +0800

    add lib path

--- setup_env.py
+++ setup_env.py
@@ -4,7 +4,11 @@ os.environ['OPENNI2_INCLUDE'] = os.path.join(
     os.path.dirname(__file__), 'OpenNI2', 'Include')
 os.environ['OPENNI2_REDIST'] = os.path.join(
     os.path.dirname(__file__), 'OpenNI2', 'Redist')
+os.environ['OPENNI2_REDIST64'] = os.path.join(
+    os.path.dirname(__file__), 'OpenNI2', 'Redist')
 os.environ['NITE2_INCLUDE'] = os.path.join(
     os.path.dirname(__file__), 'NiTE', 'Include')
+os.environ['NITE2_REDIST'] = os.path.join(
+    os.path.dirname(__file__), 'NiTE', 'Redist')
 os.environ['NITE2_REDIST64'] = os.path.join(
     os.path.dirname(__file__), 'NiTE', 'Redist')
